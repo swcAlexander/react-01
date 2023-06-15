06d7566aadb9920e41bf6d6565c18651694cc56e
Alexander Klymuk <swcAlexander@gmail.com>
2023-06-15 16:29:34 +0300

    add full version

--- src/App.js
+++ src/App.js
@@ -1,30 +1,20 @@
-import Painting from "./components/Painting";
+import PaintingList from "./components/PaintingList";
 import paintings from "./components/paintings.json"
+import Section from "./components/Section";
 
- export default function App() {
+export default function App() {
+  // const isOnline = true;
   return (
+    
     <div>
-      <Painting
-        imageUrl={paintings[0].url}
-        title={paintings[0].title}
-        author={paintings[0].author.tag}
-        profileUrl={paintings[0].author.profileUrl}
-        price={paintings[0].price}
-      />
-      <Painting
-        imageUrl={paintings[1].url}
-        title={paintings[1].title}
-        author={paintings[1].author.tag}
-        profileUrl={paintings[1].author.profileUrl}
-        price={paintings[1].price}
-      />
-      <Painting
-        imageUrl={paintings[2].url}
-        title={paintings[2].title}
-        author={paintings[2].author.tag}
-        profileUrl={paintings[2].author.profileUrl}
-        price={paintings[2].price}
-      />
+      {/* {isOnline && 'Онлайн'} */}
+      {/* { isOnline ? 'Онлайн' : 'Офлайн'} */}
+
+      
+      <Section title="Топ неділі">
+        <PaintingList items={paintings} />
+      </Section>
+      <Section/>  
     </div>
   );
 }
--- src/components/Painting.jsx
+++ src/components/Painting.jsx
@@ -4,7 +4,8 @@ const Painting = ({
     title,
     profileUrl,
     author,
-    price
+    price,
+    quantity
 }) => {
     return (<div>
         <img src={imageUrl} alt={title} width={480} />
@@ -13,16 +14,17 @@ const Painting = ({
             Автор: <a href={profileUrl}>{author}</a>
         </p>
         <p>Ціна {price} кредитів</p>
-        <p>Доступність: закінчується, або є в наявності </p>
+        <p>Доступність: {quantity > 10 ? 'є в наявності' : 'закінчується'}</p>
         <button type="button">Додати в кошик</button>
     </div>);
 }
 
 Painting.propTypes = {
-    imageUrl: PropTypes.string,
-    title: PropTypes.string,
-    profileUrl: PropTypes.string,
-    author: PropTypes.string,
-    price: PropTypes.number,
+    imageUrl: PropTypes.string.isRequired,
+    title: PropTypes.string.isRequired,
+    // profileUrl: PropTypes.string.isRequired,
+    author: PropTypes.string.isRequired,
+    price: PropTypes.number.isRequired,
+    quantity: PropTypes.number.isRequired,
 }
 export default Painting;
